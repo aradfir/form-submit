2d265940c17d7bd83d75e3317c618c0750238f5d
Arad Firouzkouhi <aradfir@gmail.com>
2022-11-20 15:41:28 +0330

    Refactored client into smaller functions

--- client/client.go
+++ client/client.go
@@ -3,7 +3,6 @@ package main
 import (
 	pb "FormSubmit/grpc"
 	"context"
-	"flag"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,35 +14,39 @@ const (
 	addr = "localhost:8080"
 )
 
-func main() {
-	flag.Parse()
-	// Set up a connection to the server.
+func initalizeConnection() (*grpc.ClientConn, pb.FormSubmitClient) {
 	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
-	c := pb.NewFormSubmitClient(conn)
+
+	return conn, pb.NewFormSubmitClient(conn)
+}
+func fillForm() *pb.FormData {
+	form := pb.FormData{}
+	fmt.Println("Enter your First Name:")
+	fmt.Scanf("%s", &form.FirstName)
+	fmt.Println("Enter your Last Name:")
+	fmt.Scanf("%s", &form.LastName)
+	fmt.Println("Enter your Email:")
+	fmt.Scanf("%s", &form.Email)
+	fmt.Println("Enter your Age:")
+	fmt.Scanf("%d", &form.Age)
+	fmt.Println("Enter your Height (m):")
+	fmt.Scanf("%f", &form.Height)
+	return &form
+}
+func main() {
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-
+	conn, c := initalizeConnection()
+	defer conn.Close()
 	anotherUser := "y"
 	for anotherUser == "y" {
-
-		form := pb.FormData{}
-		fmt.Println("Enter your First Name:")
-		fmt.Scanf("%s", &form.FirstName)
-		fmt.Println("Enter your Last Name:")
-		fmt.Scanf("%s", &form.LastName)
-		fmt.Println("Enter your Email:")
-		fmt.Scanf("%s", &form.Email)
-		fmt.Println("Enter your Age:")
-		fmt.Scanf("%d", &form.Age)
-		fmt.Println("Enter your Height (m):")
-		fmt.Scanf("%f", &form.Height)
+		form := fillForm()
 		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-		r, err := c.SubmitForm(ctx, &form)
+		r, err := c.SubmitForm(ctx, form)
 		if err != nil {
 			log.Printf("could not submit form: %v\n", err)
 		} else {
