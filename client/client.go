4586fa54726abdb97ec13f6e6bc711cf9fa9b2d8
Arad Firouzkouhi <aradfir@gmail.com>
2022-11-22 17:41:00 +0330

    refactored client

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -17,8 +18,6 @@ type defaultConfig struct {
 	DefaultPort uint
 }
 
-var config defaultConfig
-
 func initializeConnection(host string, port uint) (*grpc.ClientConn, pb.FormSubmitClient) {
 
 	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", host, port), grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -30,51 +29,58 @@ func initializeConnection(host string, port uint) (*grpc.ClientConn, pb.FormSubm
 }
 func fillForm(args []string) *pb.FormData {
 	form := pb.FormData{}
-	//fmt.Println("Enter your First Name:")
 	fmt.Sscanf(args[0], "%s", &form.FirstName)
-	//fmt.Println("Enter your Last Name:")
 	fmt.Sscanf(args[1], "%s", &form.LastName)
-	//fmt.Println("Enter your Email:")
 	fmt.Sscanf(args[2], "%s", &form.Email)
-	//fmt.Println("Enter your Age:")
 	fmt.Sscanf(args[3], "%d", &form.Age)
-	//fmt.Println("Enter your Height (m):")
 	fmt.Sscanf(args[4], "%f", &form.Height)
 	return &form
 }
-
-func RunClient(command *cobra.Command, args []string) {
+func viperSetup() (*defaultConfig, error) {
+	var config *defaultConfig
 	viper.SetConfigType("json")
 	viper.SetConfigFile("./defaults.json")
 	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("Error reading default config! aborting...")
-		return
+		return nil, err
 	}
-	err = viper.Unmarshal(&config)
+	err = viper.Unmarshal(config)
 	if err != nil {
-		fmt.Println("Error parsing default config! aborting...")
-		return
+		return nil, err
 	}
-	// Contact the server and print out its response.
-	host, _ := command.Flags().GetString("host")
+	return config, nil
+}
+func getHostAndPort(flags *pflag.FlagSet, config *defaultConfig) (string, uint) {
+	host, _ := flags.GetString("host")
 	if host == "" {
 		host = config.DefaultHost
 	}
-	port, _ := command.Flags().GetUint("port")
+	port, _ := flags.GetUint("port")
 	if port == 0 {
 		port = config.DefaultPort
 	}
+	return host, port
+}
+func RunClient(command *cobra.Command, args []string) {
+	config, err := viperSetup()
+	if err != nil {
+		log.Fatal("Error reading config (defaults.json)! aborting...")
+		return
+	}
+
+	// Contact the server and print out its response.
+	host, port := getHostAndPort(command.Flags(), config)
 	form := fillForm(args)
 	conn, c := initializeConnection(host, port)
 	defer conn.Close()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 	r, err := c.SubmitForm(ctx, form)
 	if err != nil {
 		log.Printf("could not submit form: %v\n", err)
 	} else {
 		log.Printf("Status:%v, details:%v\n", r.GetSuccess(), r.GetDetails())
 	}
-	defer cancel()
+
 }
